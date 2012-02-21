a63853986e12a3ffbfc6e7fbdb2c95b9092d7310
Shadley Wentzel <shad6ster@gmai.com>
2012-02-21 11:57:15 +0200

    Removed configs

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Battleships::Application.config.secret_token = 'e4730587249bde0eb158be0b3de54265549c97d5a504cb9eb44063349516e151dd8ea6ec214a75e0f353d45716107e5242f87727762844ac6744c462bdf4519c'
